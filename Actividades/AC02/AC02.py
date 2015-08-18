40078749c0da9aa6522109e57ca2019807e3df3c
felipehv <fahaase@uc.cl>
2015-08-18 11:14:35 -0300

    modified:   Actividades/AC02/AC02.py

--- Actividades/AC02/AC02.py
+++ Actividades/AC02/AC02.py
@@ -29,13 +29,18 @@ class Inalambrico(Audifono):
 	def __init__(self,rango,**kwargs):
 		super().__init__(**kwargs)
 		self.rango = rango
+		self.conectado = False
 
 	def escuchar(self,cancion):
-		print(('La cancion {} esta siendo reproducida en un audifono inalambrico.').format(cancion))
+		if self.conectado:
+			print(('La cancion {} esta siendo reproducida en un audifono inalambrico.').format(cancion))
+		else:
+			print('Audifono desconectado')
 
 	def conectar(self,rango):
 		if rango <= self.rango:
 			print('Conectado')
+			self.conectado = True
 		else:
 			print('Error, el reproductor esta muy lejos.')
 
@@ -45,7 +50,8 @@ class Bluetooth(Inalambrico):
 		self.identificador = identificador
 
 	def escuchar(self,cancion):
-		print(('La cancion {} esta siendo reproducida en un audifono Bluetooth').format(cancion))
+		if self.conectado:
+			print(('La cancion {} esta siendo reproducida en un audifono Bluetooth').format(cancion))
 
 if __name__ == '__main__':
 	audifono = Audifono(frecMin=20,frecMax=18000,impedancia=15,VolumenMax=100)
@@ -54,8 +60,8 @@ if __name__ == '__main__':
 	bluetooth = Bluetooth(frecMin=18,frecMax=15000,\
 					impedancia=10,VolumenMax=60,rango=10,identificador=2233)
 	audifono.escuchar('Money')
-	inalambrico.conectar(8)
 	inalambrico.conectar(12)
+	inalambrico.conectar(8)
 	inalambrico.escuchar('Money')
 	bluetooth.conectar(2)
 	bluetooth.conectar(120)
