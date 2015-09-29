7485d62c2f558abf572c1aed3df1590030d6d4d9
Felipe Haase <fahaase@uc.cl>
2015-09-29 14:25:44 -0300

    AC13

--- Actividades/AC13/AC13.py
+++ Actividades/AC13/AC13.py
@@ -1,6 +1,8 @@
 import unittest
 from main import *
 def Verificador(rut):
+	verificador = rut[-1]
+	rut = rut[0:-2]
 	rut = rut[::-1]
 	suma = 0
 	numeros = [2,3,4,5,6,7]
@@ -8,12 +10,12 @@ def Verificador(rut):
 		suma += int(rut[i]) * numeros[i%6]
 
 	resto = suma%11
-	if num == 10:
-		return 'k'
-	elif numm == 11:
-		return "0"
-	return str(11-resto)
-
+	if 11-resto == 10:
+		num = 'k'
+	elif 11-resto == 11:
+		num= "0"
+	num= str(11-resto)
+	return num == verificador
 
 class AC13(unittest.TestCase):
 	
@@ -23,21 +25,19 @@ class AC13(unittest.TestCase):
 			self.archivos.append(Corrector(n))
 
 	def test_revisar_nombre(self):
-		"""
 		self.assertTrue(self.archivos[0].revisar_nombre())
 		self.assertTrue(self.archivos[1].revisar_nombre())
 		self.assertTrue(self.archivos[2].revisar_nombre())
 		self.assertFalse(self.archivos[3].revisar_nombre())
-"""
+
 	def test_revisarFormato(self):
-		"""
 		self.assertTrue(self.archivos[0].revisar_formato(self.archivos[0].nombre.split('.')[1]))
 		self.assertTrue(self.archivos[1].revisar_formato(self.archivos[1].nombre.split('.')[1]))
 		self.assertTrue(self.archivos[2].revisar_formato(self.archivos[2].nombre.split('.')[1]))
 		self.assertFalse(self.archivos[3].revisar_formato(self.archivos[3].nombre.split('.')[1]))
-"""
+
 	def test_revisarVerificador(self):
-		rut=[i.nombre.split('_')[1] for i in self.archivos]
+		rut=[i.nombre.split('_')[0] for i in self.archivos]
 		self.assertEqual(self.archivos[0].revisar_verificador(rut[0]),Verificador(rut[0]))
 		self.assertEqual(self.archivos[1].revisar_verificador(rut[1]),Verificador(rut[1]))
 		self.assertEqual(self.archivos[2].revisar_verificador(rut[2]),Verificador(rut[2]))
@@ -52,8 +52,11 @@ class AC13(unittest.TestCase):
 	def test_getPalabras(self):
 		pass
 
-	def test_getDescuento(self):
-		pass
+	def test_get_descuento(self):
+	    for i in range(len(self.archivos)):
+	        self.assertIsInstance(self.archivos[i].get_descuento, int)
+	        if self.archivos[i].revisar_nombre() and self.archivos[i].palabras < 500:
+	            self.assertEqual(self.archivos[i].get_descuento, 1.5)
 
 	def tearDown(self):
 		pass
