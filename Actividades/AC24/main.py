796b37edb8ca7d0b02d90cd7cbc055f46ee25798
Felipe Haase <fahaase@uc.cl>
2015-11-05 11:16:52 -0300

    AC24

--- Actividades/AC24/main.py
+++ Actividades/AC24/main.py
@@ -5,40 +5,89 @@ from gato import Gato, sys
 
 class Service:
     def __init__(self):
-        pass
+        self.host = socket.gethostname()
+        self.port = 10001
 
     def escuchar(self):
-        pass
+        data = self.socket.recv(1024)
+        data = data.decode('ascii')
+        print("El otro jugador puso su ficha en {}".format(data))
 
     def enviar(self, mensaje):
-        pass
+        if not self.turno:
+            print('Presiona <enter> para actualizar y ver si es tu turno')
+            return
+        mensaje = mensaje.strip()
+        if self.turno == self.gato.turno and 0 <= int(mensaje[0]) < 3 and 0 <= int(mensaje[2]) < 3:
+            if self.gato.estado[int(mensaje[0])][int(mensaje[2])] == ' ':
+                self.gato.editar_posicion(mensaje.split(','))
+                s_otro, addr = self.socket.accept()
+                s_otro.send(mensaje.encode('ascii'))
+            else:
+                print("Posicion ocupada")
 
+        else:
+            print("Espera tu turno, tu eres {}".format(self.turno))
 
 class Cliente(Service):
     def __init__(self, gato):
-        pass
+        super().__init__()
+        self.gato = gato
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.turno = "O"
+        self.socket.connect((self.host,self.port))
+
+    def enviar(self, mensaje):
+        print(self.gato)
+        if not self.turno:
+            print('Presiona <enter> para actualizar y ver si es tu turno')
+            return
+        mensaje = mensaje.strip()
+        if self.turno == self.gato.turno and 0 <= int(mensaje[0]) < 3 and 0 <= int(mensaje[2]) < 3:
+            if self.gato.estado[int(mensaje[0])][int(mensaje[2])] == ' ':
+                self.gato.editar_posicion(mensaje.split(','))
+                self.socket.send(mensaje.encode('ascii'))
+            else:
+                print("Posicion ocupada")
+
+        else:
+            print("Espera tu turno, tu eres {}".format(self.turno))
 
 
 class Servidor(Service):
     def __init__(self, gato):
-        pass
+        super().__init__()
+        self.turno = "X"
+        self.gato = gato
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host,self.port))
+        self.socket.listen(10)
 
     def aceptar(self):
-        pass
+        cont = 0
+        client, address = self.socket.accept()      
+        print("Obtuvimos una conexión desde %s" % str(address))
+        for i in range(10):
+            data = client.recv(1024)
+            data = data.decode('ascii')
+            print("El jugador O jugo su ficha en {}".format(data))
+
+        client.close()
+
 
 
 if __name__ == "__main__":
-
+    gato = Gato()
     pick = input("Ingrese X si quiere ser servidor o O si desea ser cliente: ")
     if pick == "X":
-        server = Servidor()
+        server = Servidor(gato)
         server.aceptar()
         while True:
             mensaje = input("Jugador {0} debe ingresar la posicion en que desea jugar".format(server.gato.turno))
             server.enviar(mensaje)
 
     elif pick == "O":
-        client = Cliente()
+        client = Cliente(gato)
         escuchador = threading.Thread(target=client.escuchar)
         escuchador.daemon = True
         escuchador.start()
