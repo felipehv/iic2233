37aeeb85186ef010026f4217d9729a92b2bb22ab
Felipe Haase <fahaase@uc.cl>
2015-11-06 16:37:17 -0300

    update T05

--- Tareas/T05/classes.py
+++ Tareas/T05/classes.py
@@ -1,47 +1,91 @@
 from PyQt4 import QtGui, uic, QtCore
 import random
 import threading
-class Player:
+from math import atan2, degrees, pi
+import os
+class Player(QtGui.QLabel):
 
-	def __init__(self,x,y,game):
-		self.health = 1000
-		self.ammo = 20
-		self.pos = (x,y)
-		self.game = game
+    def __init__(self,game):
+        super().__init__(game)
+        self.health = 1000
+        self.ammo = 20
+        self.game = game
+        self.vector = 0,0
 
-	def move(self,x,y):
-		self.label1.move(x,y)
+        #self.setGeometry(40, 40, 50, 50)
+        self.setScaledContents(True)
+        #use full ABSOLUTE path to the image, not relative
+        self.pixmap = QtGui.QPixmap(os.getcwd() + "/sprites/player.png")
+        self.setPixmap(self.pixmap)
 
-	def attack(self):
-		pass
+    def mover(self, ort = False):
+        self.calcular_vector(self.game.mx,self.game.my)
+        self.rotate()
+        if not ort:
+            xx,yy = self.vector
+        else:
+            xx,yy = self.vectorOrtogonal()
+        x = self.x()
+        y = self.y()
+        self.move(x+xx*6,y+yy*6)
 
-class Zombie():
-	def __init__(self,x,y):
-		pass
+    def calcular_vector(self,m_x,m_y):
+        x = self.x()
+        y = self.y()
+        x,y = m_x - x, m_y - y
+        n = (x**2 + y**2)**0.5
+        if n == 0:
+            return 0,0
+        self.persona.vector = x/n , y/n
 
-	def attack(self,player):
-		player.health -= random.randint()
+    def vectorOrtogonal(self):
+        vector = -self.vector[1],self.vector[0]
+        return vector
 
-	def recta(self,player):
-		player_pos = player.pos()
-"""
-class Worker(QtCore.QThread):
+    def rotate(self):
+        theta = degrees(atan2(self.vector[1],self.vector[0]) + pi/2)
+        self.pixmap = QtGui.QPixmap(os.getcwd() + "/sprites/player.png")
+        self.pixmap = self.pixmap.transformed(QtGui.QTransform().rotate(theta))
+        self.setPixmap(self.pixmap)
 
-    def __init__(self, queue, interval):
-        super().__init__()
-        self.queue = queue
-        self.interval = interval
+    def calcular_vector(self,m_x,m_y):
+        x = self.x()
+        y = self.y()
+        x,y = m_x - x, m_y - y
+        n = (x**2 + y**2)**0.5
+        self.vector = x/n , y/n
 
-    def __del__(self):
-        self.exiting = True
-        self.wait()
+    def caminar(self):
+        pass
 
-    def run(self):
-        while True:
-            if len(self.queue) > 0:
-                to_do = self.queue.popleft()
-                self.emit(QtCore.SIGNAL("do"), to_do)
-                time.sleep(self.interval)
-            else:
-                time.sleep(0.01)
-"""
+class Zombie(QtGui.QLabel):
+    def __init__(self,game):
+        super().__init__(game)
+        self.health = 1000
+        self.game = game
+        self.vector = 0,0
+        #self.setGeometry(10, 10, 50, 50)
+        #self.setScaledContents(True)
+        #use full ABSOLUTE path to the image, not relative
+        self.pixmap = QtGui.QPixmap(os.getcwd() + "/sprites/player.png")
+        self.setPixmap(self.pixmap)
+
+    def mover(self):
+        self.vector()
+        xx,yy = self.vector
+        x = self.x()
+        y = self.y()
+        self.move(x+xx*2,y+yy*2)
+
+    def attack(self,player):
+        player.health -= random.randint()
+
+    def calcular_vector(self,m_x,m_y):
+        x = self.game.persona.x()
+        y = self.game.persona.y()
+        x,y = m_x - x, m_y - y
+        n = (x**2 + y**2)**0.5
+        self.persona.vector = x/n , y/n
+
+    def mover():
+        self.move(self.x*6,self.y*6)
--- Tareas/T05/main.py
+++ Tareas/T05/main.py
@@ -2,7 +2,7 @@ from PyQt4 import QtGui, uic, QtCore, QtCore
 import threading
 from classes import *
 import os
-from math import atan2
+from math import atan2, degrees
 
 class MainWindow(QtGui.QMainWindow):
 
@@ -21,6 +21,22 @@ class MainWindow(QtGui.QMainWindow):
     def juego(self):
         self.game_widget.juego()
 
+    def keyPressEvent(self,event):
+        #sender = self.sender()
+        print(event)
+        if event.text() == "w":
+            self.game_widget.persona.mover()
+        elif event.text() == "d":
+            self.game_widget.persona.mover(True)
+        elif event.text() == "p":
+            self.setCentralWidget(self.mainwindow.pause_widget)
+    """
+    def keyPressEvent(self,event):
+        #sender = self.sender()
+        print(event.text())
+        if event.text() == "w":
+            self.game_widget.persona.mover()
+    """
 class MenuWidget(QtGui.QWidget):
 
     def __init__(self,main):
@@ -31,7 +47,7 @@ class MenuWidget(QtGui.QWidget):
     def init_GUI(self):
         self.start_button = QtGui.QPushButton('Comenzar el juego', self)
         self.start_button.resize(self.start_button.sizeHint())
-        self.start_button.move(5, 70)
+        self.start_button.move(400,400)
         self.start_button.clicked.connect(self.empezar)
 
     def empezar(self):
@@ -40,7 +56,15 @@ class MenuWidget(QtGui.QWidget):
 
 class PauseWidget(QtGui.QWidget):
     def __init__(self,main):
-        pass
+        super().__init__()
+        self.init_GUI()
+        self.MainWindow = main
+
+    def init_GUI(self):
+        self.pause_button = QtGui.QLabel(self)
+        self.pause_button.setText("Juego en pausa <enter> para continuar.")
+        self.pause_button.resize(self.pause_button.sizeHint())
+        self.pause_button.move(400,400)
 
 class GameWidget(QtGui.QWidget):
     def __init__(self,main):
@@ -48,74 +72,55 @@ class GameWidget(QtGui.QWidget):
         self.init_GUI() 
         self.setMouseTracking(True)
         self.mainwindow = main
+        self.zombies = []
 
     def init_GUI(self):
-        self.persona = QtGui.QLabel(self)
-        self.persona.setGeometry(10, 10, 50, 50)
-        #use full ABSOLUTE path to the image, not relative
-        self.persona.setPixmap(QtGui.QPixmap(os.getcwd() + "/sprites/pacman.png"))
+        self.persona = Player(self)
+        self.persona.move(50,50)
 
-    def mover(self):
-        xx,yy = self.vector
+    def keyPressEvent(self,event):
+        #sender = self.sender()
+        print(event)
+        if event.text() == "p":
+            self.mainwindow.setCentralWidget(self.mainwindow.pause_widget)
+
+        #self.persona = QtGui.QLabel(self)
+        #self.persona.setGeometry(10, 10, 50, 50)
+        #self.persona.setScaledContents(True)
+        #use full ABSOLUTE path to the image, not relative
+        #self.persona.setPixmap(QtGui.QPixmap(os.getcwd() + "/sprites/player.png"))
+
+    def mover(self,ortogonal = False):
+        if not ortogonal:
+            xx,yy = self.vector
+        else:
+            xx,yy = self.vectorOrtogonal()
         #sender = self.sender()
         x = self.persona.x()
         y = self.persona.y()
         self.persona.move(x+xx*2,y+yy*2)
 
-    def calcular_vector(self,m_x,m_y):
-        x = self.persona.x()
-        y = self.persona.y()
-        x,y = m_x - x, m_y - y
-        n = (x**2 + y**2)**0.5
-        self.vector = x/n , y/n
-
     def vectorOrtogonal(self):
-        vector = self.vector()
+        vector = self.vector[1],self.vector[0]
+        return vector
 
-    def rotar(self):
-        pass
-        #pixmap = pixmap.transformed(QtGui.QTransform().rotate(theta))
+    def keyPressEvent(self,event):
+        sender = self.sender()
+        print(event)
+        if event.text() == "w":
+            self.persona.mover()
+        elif event.text() == "p":
+            self.mainwindow.setCentralWidget(self.mainwindow.pause_widget)
 
     def mousePressEvent(self,event):
         pass
         #print('hola')
 
-    def keyPressEvent(self,event):
-        #sender = self.sender()
-        print(event.text())
-        if event.text() == "w":
-            self.mover()
-
     def mouseMoveEvent(self,event):
-        self.calcular_vector(event.x(),event.y())
-        #self.rotar()
-
-
-    """
-    def agregar_player(self, filename, x, y, theta=0):
-        path = os.path.join(os.path.dirname(__file__), "sprites", filename)
-        pixmap = QtGui.QPixmap(path)
-        label = self.grid.grid_2.itemAtPosition(x, y).widget()
-        pixmap = pixmap.scaled(self.img_size, QtCore.Qt.KeepAspectRatio)
-
-        if theta != 0:
-            pixmap = pixmap.transformed(QtGui.QTransform().rotate(theta))
-
-        label.setPixmap(pixmap)
-
-    def agregar_imagen(self,filename, x, y):
-        path = os.path.join(os.path.dirname(__file__), "assets", filename)
-        pixmap = QtGui.QPixmap(path)
-        label = self.grid_2.itemAtPosition(x, y)
-        pixmap = pixmap.scaled(self.img_size, QtCore.Qt.KeepAspectRatio)
-
-        #if theta != 0:
-        #    pixmap = pixmap.transformed(QtGui.QTransform().rotate(theta))
-        #if reflection:
-        #    pixmap = pixmap.transformed(QtGui.QTransform().scale(-1, 1))
-
-        label.setPixmap(pixmap)
-    """
+        self.mx = event.x()
+        self.my = event.y()
+        self.persona.calcular_vector(event.x(),event.y())
+        self.persona.rotate()
 
 if __name__ == '__main__':
     # Inicializamos la aplicación de la misma forma que 
